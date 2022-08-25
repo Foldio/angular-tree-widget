d58e0753ae241de9d20f41edbc5c3db673946784
Martin Petit <martin@igloo.be>
2022-08-25 11:59:32 +0200

    nodeFilter: make filter diacritic insensitive

--- dist/angular-tree-widget.js
+++ dist/angular-tree-widget.js
@@ -7,6 +7,35 @@
 (function (angular) {
     'use strict';
 
+    function transformText(str) {
+        if (str && str.search(/[\xC0-\xFF]/g) > -1) {
+            str = str
+                    .replace(/[\xC0-\xC5]/g, "A")
+                    .replace(/[\xC6]/g, "AE")
+                    .replace(/[\xC7]/g, "C")
+                    .replace(/[\xC8-\xCB]/g, "E")
+                    .replace(/[\xCC-\xCF]/g, "I")
+                    .replace(/[\xD0]/g, "D")
+                    .replace(/[\xD1]/g, "N")
+                    .replace(/[\xD2-\xD6\xD8]/g, "O")
+                    .replace(/[\xD9-\xDC]/g, "U")
+                    .replace(/[\xDD]/g, "Y")
+                    .replace(/[\xDE]/g, "P")
+                    .replace(/[\xE0-\xE5]/g, "a")
+                    .replace(/[\xE6]/g, "ae")
+                    .replace(/[\xE7]/g, "c")
+                    .replace(/[\xE8-\xEB]/g, "e")
+                    .replace(/[\xEC-\xEF]/g, "i")
+                    .replace(/[\xF1]/g, "n")
+                    .replace(/[\xF2-\xF6\xF8]/g, "o")
+                    .replace(/[\xF9-\xFC]/g, "u")
+                    .replace(/[\xFE]/g, "p")
+                    .replace(/[\xFD\xFF]/g, "y");
+        }
+
+        return str.toLowerCase();
+      }
+
     angular.module('TreeWidget', ['ngAnimate', 'RecursionHelper','uuid4'])
         .directive('tree', ['uuid4', function (uuid4) {
             return {
@@ -61,7 +90,13 @@
                 var recursiveFilter = function (nodes, filter) {
                     var filtered = [];
                     angular.forEach(nodes, function (node) {
-                        if ($filter('filter')([node], filter).length > 0) {
+                        if ($filter('filter')([node], filter, (actual, expected) => {
+                            if (typeof actual !== "string") {
+                                return false
+                            }
+
+                            return (transformText(actual)).includes(transformText(expected))
+                        }).length > 0) {
                             filtered.push(node);
                         } else if (angular.isArray(node.children) && node.children.length > 0) {
                             var internal = recursiveFilter(node.children, filter);
